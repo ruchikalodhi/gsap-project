06a1cf315ced3a96614268098d0b3ca611adfd79
ruchikalodhi <183547614+ruchikalodhi@users.noreply.github.com>
2026-08-19 18:17:56 +0530

    herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import gsap from 'gsap';
 import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from './Components/Navbar.jsx';
+import Hero from './Components/hero.jsx';
 gsap.registerPlugin(ScrollTrigger, SplitText); //ready to use globally 
 
 import React from 'react';
 
 const App = () => {
   return (
-    <main> <Navbar /> </main>
+    <main> 
+      <Navbar /> 
+      <Hero />
+    </main>
   );
 }   
 export default App;
