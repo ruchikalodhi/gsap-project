732466289f138715399b3fa10a865656060ea18d
ruchikalodhi <183547614+ruchikalodhi@users.noreply.github.com>
2026-08-21 05:30:06 +0530

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero.jsx'
 import Cocktails from './Components/Cocktails.jsx'
+import About from './Components/About.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ const App = () => {
 	 <Navbar />
 	 <Hero />
 	 <Cocktails />
+   <About />
 	</main>
  )
 }
